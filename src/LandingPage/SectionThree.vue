e68c820329781730464dd4da785545bf57566ed8
PotoVic <wiktor.okonski39@gmail.com>
2025-03-04 17:05:26 +0100

    :construction: Test?

---
+++ src/LandingPage/SectionThree.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="SectionThree">
+        <div class="SectionThreeImage"></div>
+        <div class="SectionThreeTextBox">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    @import 'SectionThreeStyle.scss';
+</style>
