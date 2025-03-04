ac3a9396c7a6b9572c31fe65306a5e9a4786b384
Wiktor Okonski <159134437+PotoVic@users.noreply.github.com>
2025-03-04 15:35:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fed24d-grafiska-verktyg-individuell-PotoVic/",
+  base: "/fed24d-grafiska-verktyg-surikaterna/",
   plugins: [
     vue(),
     vueDevTools(),
