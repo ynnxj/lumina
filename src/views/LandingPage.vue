d14865f03b34e4e3a559f6effc6b38d5b160449c
Kristofer <kristofer.sven.ellstrom@gmail.com>
2025-03-05 13:18:31 +0100

    🚧 feat: completed desktop layout for SectionTwoHistory

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,11 +1,11 @@
 <script setup>
 import SectionThree from '@/components/SectionThree.vue';
 import SectionFive from '@/components/SectionFive.vue';
+import HistorySection from '@/components/SectionTwoHistory.vue';
 </script>
 
-
-
 <template>
+    <HistorySection/>
     <SectionThree />
     <SectionFive />
 </template>
