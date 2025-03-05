5dd49f6469042022f53463eaa3db826ad6f5e462
PotoVic <wiktor.okonski39@gmail.com>
2025-03-05 12:24:53 +0100

    :construction: new views files, for orderpage and menupage

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,26 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: LandingPage,
     },
-  //  {
-  //     path: '/about',
-  //    name: 'about',
+ /*  {
+      path: '/order',
+      name: 'order',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-   //   component: () => import('../views/AboutView.vue'),
-  //  },
+     component: () => import('../views/Orderpage.vue'),
+   },
+   {
+    path: '/menu',
+    name: 'menu',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+   component: () => import('../views/MenuPage.vue'),
+ },*/
   ],
 })
 
