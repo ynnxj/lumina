bb4648f85bdd01607b0b0595c5b32f3e92f9dceb
PotoVic <wiktor.okonski39@gmail.com>
2025-03-05 10:42:20 +0100

    :construction: Added font styles, Paragraph text, and Image

--- src/LandingPage/SectionThree.vue
+++ src/LandingPage/SectionThree.vue
@@ -1,12 +1,22 @@
+<script setup>
+
+</script>
+
 <template>
     <div class="SectionThree">
-        <div class="SectionThreeImage"></div>
+        <div class="SectionThreeImage">
+           <img src="./images/lumina-classic.jpg" alt="Lumina Classic">
+        </div>
         <div class="SectionThreeTextBox">
-
+            <h1>Luminas Signatur</h1>
+            <p>Lumina Soleil är en elegant och smakrik cocktail som fångar essensen av Lumina Bar. Mörk premiumrom möter friskheten från färskpressad apelsin- och citronjuice, balanserat med djupet från honungssirap och Grand Marnier.</p>
+            <p>Ett stänk apelsinbitters fulländar denna sofistikerade drink, där sötma och syra förenas i perfekt harmoni. Lumina Soleil är en hyllning till ljusets lekfullhet – en uppfriskande och exklusiv upplevelse för finsmakaren.</p>
+            <button>Drinkmeny</button>
         </div>
     </div>
 </template>
 
 <style scoped>
     @import 'SectionThreeStyle.scss';
+
 </style>
