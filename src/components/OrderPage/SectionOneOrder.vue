c0add9eb79fb52893031c983fd5c705b533d5120
Alicia <a.skogblad@gmail.com>
2025-03-06 12:08:24 +0100

    :construction: add first draft for booking p1

---
+++ src/components/OrderPage/SectionOneOrder.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <img src="/src/assets/images/images-and-videos/booking-page/stir.jpg" width="3500" height="2333" alt="Cocktail tillreds av bartender i dämpad belysning.">
+
+    <h2>Boka</h2>
+  </main>
+</template>
+
+<style scoped>
+  @import 'SectionOneOrderStyle.scss';
+</style>
