5149a7837e0632bc1f7e76df691c4d2cfe22de41
Alicia <a.skogblad@gmail.com>
2025-03-06 10:02:39 +0100

    🔀 moved files to correct map

---
+++ src/components/LandingPage/SectionOne.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<template>
+  <main>
+    <!-- Mobile/Tablet video -->
+    <video width="768" height="1048" autoplay muted loop class="video-small-hp" src="/src/assets/images/images-and-videos/home-page/mobile-and-tablet-video/tablet-and-mobile-video.mp4" type="video/mp4"></video>
+
+    <!-- Desktop video -->
+    <video width="1440" height="924" autoplay muted loop class="video-hp" src="/src/assets/images/images-and-videos/home-page/desktop-video/desktop-video.mp4" type="video/mp4"></video>
+
+    <div class="container">
+      <h1>
+        <img width="279" height="86" src="/src/assets/images/images-and-videos/logos/LUMINA-logo.svg" alt="Logotyp för Lumina">
+      </h1>
+
+      <div class="btn-container">
+        <button class="meny">Meny</button>
+        <button class="book">Boka bord</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  @import 'SectionOneStyle.scss';
+</style>
