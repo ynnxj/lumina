c0970f9d777494442e9f3ee4a05f9062c2bb219c
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-03-06 11:43:42 +0100

    ♻️ refactor(App.vue): Change view to enable navigation
    Change from <LandingPage/> to <RouterView/> to enable page navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import LandingPage from './views/LandingPage.vue';
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-    <LandingPage/>
+    <RouterView/>
 </template>
 
 <style scoped>
