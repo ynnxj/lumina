87077914f215ab9de5c39da1c7a1311442dd87f5
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-03-06 11:52:43 +0100

    🚧 feat(MenuPage): add section one
    Work in progress.

---
+++ src/components/MenuPage/SectionOne.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <img src="/src/assets/images/images-and-videos/menu-page/jigger-pour.jpg" alt="A male bartender with short dark brown hair pouring liquid into a jigger behind a bar">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    img {
+        width: 100vw;
+    }
+</style>
