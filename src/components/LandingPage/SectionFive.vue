27fe8e9503039eae4e3bb2ee758790372ec3567c
PotoVic <wiktor.okonski39@gmail.com>
2025-03-06 10:52:52 +0100

    :art: added styling to page 5 + changes in page 3

--- src/components/LandingPage/SectionFive.vue
+++ src/components/LandingPage/SectionFive.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="SectionFive">
         <div class="SectionFiveTextBox">
-            <h1>Besök Lumina</h1>
+            <h2>Besök Lumina</h2>
             <p>Lorem ipsum dolor sit amet consectetur. Elementum amet mattis in placerat placerat elit maecenas. Fermentum placerat volutpat diam purus justo. Accumsan eget vehicula eget amet arcu nibh. Magna sit porttitor et nibh proin sagittis nullam quam lacinia. Justo est ac neque amet suspendisse pharetra lobortis tempus. Pharetra quis porta scelerisque vitae feugiat elementum pharetra at. Scelerisque vulputate dictumst pulvinar ipsum mauris feugiat. Ut lectus est lacus arcu sed. Nunc eu metus habitant sit a vitae.</p>
             <p>Lorem ipsum dolor sit amet consectetur. Elementum amet mattis in placerat placerat elit maecenas. Fermentum placerat volutpat diam purus justo. Accumsan eget vehicula eget amet arcu nibh. Magna sit porttitor et nibh proin sagittis nullam quam lacinia. Justo est ac neque amet suspendisse pharetra lobortis tempus. Pharetra quis porta scelerisque vitae feugiat elementum pharetra at. Scelerisque vulputate </p>
             <button>Boka Bord</button>
         </div>
         <div class="SectionFiveImage">
-            <img src="/src/assets/images/images-and-videos/home-page/serving.jpg" alt="">
+            <img src="/src/assets/images/images-and-videos/home-page/serving.jpg" alt="Serving Lumina">
         </div>
     </div>
 
