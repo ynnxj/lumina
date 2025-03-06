6cc94f9b772614930a0c52a74e2250a0fbcda794
Alicia <a.skogblad@gmail.com>
2025-03-06 15:41:42 +0100

    :wheelchair: add h2 to top img (logo)

--- src/components/BookPage/SectionOneBook.vue
+++ src/components/BookPage/SectionOneBook.vue
@@ -1,8 +1,10 @@
 <template>
   <main>
-    <img src="/src/assets/images/images-and-videos/booking-page/stir.jpg" width="3500" height="2333" alt="Cocktail tillreds av bartender i dämpad belysning.">
-
-    <h2>Boka</h2>
+    <div class="container">
+      <h2>
+        <img src="/src/assets/images/images-and-videos/booking-page/boka.svg" width="274" height="105" class="book-img" alt="Boka">
+      </h2>
+    </div>
   </main>
 </template>
 
