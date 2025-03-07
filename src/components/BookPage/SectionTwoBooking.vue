26473d4fe1bc3b52c9a8f78746e549a2fd7b3f15
Kristofer <kristofer.sven.ellstrom@gmail.com>
2025-03-07 01:20:16 +0100

    📌🚀 feat: started working on booking-section-under

--- src/components/BookPage/SectionTwoBooking.vue
+++ src/components/BookPage/SectionTwoBooking.vue
@@ -1,30 +1,66 @@
 <template>
     <section class="booking-page2">
-    <section class="booking-section-over">
-      <div class="booking-content">
-        <h2 class="booking-title">Boka bord</h2>
-        <p class="booking-info-over">
-          Lorem ipsum dolor sit amet consectetur. Elementum amet mattis in placerat placerat elit maecenas.
-          Fermentum placerat volutpat diam purus justo. Accumsan eget vehicula eget amet arcu nibh.
-        </p>
-        <p class="booking-info-under">
-          Magna sit porttitor et nibh proin sagittis nullam quam lacinia.
-          Justo est ac neque amet suspendisse pharetra lobortis tempus.
-          Pharetra quis porta scelerisque vitae feugiat elementum pharetra at.
-          Scelerisque vulputate dictumst pulvinar ipsum mauris feugiat.
-        </p>
-      </div>
+      <section class="booking-section-over">
+        <div class="booking-content">
+          <h2 class="booking-title">Boka bord</h2>
+          <p class="booking-info-over">
+            Lorem ipsum dolor sit amet consectetur. Elementum amet mattis in placerat placerat elit maecenas.
+            Fermentum placerat volutpat diam purus justo. Accumsan eget vehicula eget amet arcu nibh.
+          </p>
+          <p class="booking-info-under">
+            Magna sit porttitor et nibh proin sagittis nullam quam lacinia.
+            Justo est ac neque amet suspendisse pharetra lobortis tempus.
+            Pharetra quis porta scelerisque vitae feugiat elementum pharetra at.
+            Scelerisque vulputate dictumst pulvinar ipsum mauris feugiat.
+          </p>
+        </div>
   
-      <!-- Höger sida: Bild -->
-      <div class="info-book">
-        <img class="book-image" :src="bookImage" alt="Uppslagen bok liggandes ner" />
-      </div>
+        <div class="info-book">
+          <img class="book-image" :src="bookImage" alt="Uppslagen bok liggandes ner" />
+        </div>
+      </section>
+    <section class="booking-section-under">
+        <div class="booking-form">
+          <form>
+            <label for="name">Namn på bokningen*</label>
+            <input type="text" id="name" placeholder="Placeholder" />
+  
+            <label for="people">Antal personer*</label>
+            <input type="text" id="people" placeholder="Placeholder" />
+  
+            <label for="date">Önskat datum*</label>
+            <input type="text" id="date" placeholder="Placeholder" />
+  
+            <label for="time">Önskad tid*</label>
+            <input type="text" id="time" placeholder="Placeholder" />
+  
+            <label for="email">Din e-mail*</label>
+            <input type="email" id="email" placeholder="Placeholder" />
+  
+            <label for="phone">Ditt telefonnummer*</label>
+            <input type="text" id="phone" placeholder="Placeholder" />
+  
+            <div class="gdpr">
+              <input type="checkbox" id="gdpr" />
+              <label for="gdpr">GDPR Lorem ipsum dolor sit amet</label>
+            </div>
+  
+            <div class="form-buttons">
+              <button type="reset" class="btn-reset">Rensa</button>
+              <button type="submit" class="btn-submit">Skicka förfrågan</button>
+            </div>
+          </form>
+        </div>
+  
+        
+      </section>
+  
     </section>
-</section>
   </template>
   
   <script setup>
   import bookImage from "@/assets/images/images-and-videos/booking-page/book.png";
+ 
   import "./SectionTwoBooking.scss";
   </script>
   
