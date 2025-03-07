fd45f3eee722992652544fb7b4ebff4520b0dd71
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-03-07 15:52:50 +0100

    ✨feat(MenuPage.vue): update with imports
    Add import to menu page sections, header and footer. Closes #30. Closes #29

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -1,7 +1,15 @@
 <script setup>
-  import SectionOne from '@/components/MenuPage/SectionOne.vue';
+	import HeaderSection from '@/components/HeaderSection.vue';
+	import SectionOne from '@/components/MenuPage/SectionOne.vue';
+	import SectionTwo from '@/components/MenuPage/SectionTwo.vue';
+	import SectionThree from '@/components/MenuPage/SectionThree.vue';
+	import FooterSection from '@/components/FooterSection.vue';
 </script>
 
 <template>
-  <SectionOne />
+	<HeaderSection />
+	<SectionOne />
+	<SectionTwo />
+	<SectionThree />
+	<FooterSection />
 </template>
