7940a4fe5a41705b6d8ecdd3303fdd4bcbfa6bbe
Kristofer <kristofer.sven.ellstrom@gmail.com>
2025-03-09 00:42:40 +0100

    💄 fix: corrected styling issues in SectionTwoBooking, SectionTwoHistory and added footer and header to BookPage

--- src/views/BookPage.vue
+++ src/views/BookPage.vue
@@ -1,10 +1,15 @@
 <script setup>
+import HeaderSection from '@/components/HeaderSection.vue';
 import SectionOneBook from '@/components/BookPage/SectionOneBook.vue';
 import SectionTwoBooking from '@/components/BookPage/SectionTwoBooking.vue';
+import FooterSection from '@/components/FooterSection.vue';
+
 
 </script>
 
 <template>
+  <HeaderSection />
   <SectionOneBook />
   <SectionTwoBooking />
+  <FooterSection />
 </template>
