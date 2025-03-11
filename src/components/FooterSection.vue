8a7578b6940c65f6559415c8d0e6487842b831de
Kristofer <kristofer.sven.ellstrom@gmail.com>
2025-03-11 15:50:42 +0100

    🐛 fix: resolved console warning

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -32,14 +32,14 @@
     </div>
     <div class="footer-info-box">
       <h2>Socialt</h2>
-      <Nav>
+      <nav>
         <ul>
           <li><a href="#">Instagram</a></li>
           <li><a href="#">Twitter</a></li>
           <li><a href="#">Facebook</a></li>
           <li><a href="#">Tiktok</a></li>
         </ul>
-      </Nav>
+      </nav>
     </div>
   </div>
 </template>
